687b5313ecca203d61ce11ec275dc784059e7a83
AlexeiSperanski <39588160+alexeisperanski@users.noreply.github.com>
2018-11-26 14:18:30 +0300

    Replase line in file

--- DatabaseTest.py
+++ DatabaseTest.py
@@ -18,18 +18,34 @@ import math
 #return line(len(key) + 1; -1)
 
 def set_value(key: str, value: str) -> None:
+    # Функция добавляет пользователя в файл database.txt
     with open("database.txt", "a") as file:
-        file.write(key + ':' + value + "\n")
+        file.write(f"{key}:{value}\n")
     return None
 
 
 def get_value(key: str) -> str:
+    # Функция возвращает значение по ключу
     with open("database.txt", "r") as file:
         for line in file:
             if key == line.split(":")[0]:
                 return line.split(":")[1].replace("\n", "")
 
+def new_base(size: int) -> None:
+    # Функция заполняет файл database.txt пользователями
+    for num in range(size):
+        set_value(f"user{num}", f"pass{num}")
+#print(get_value("user1"))
+#new_base(30)
+#
+# with open("database.txt", "a") as file:  # open for append
+#     for line in open("database.txt", "r"):
+#         line = line.replace('user1', 'word')
+#         file.write(line)
+# with open("database.txt", "w") as file:
+#   print()
+with open("database.txt", "r") as file:
+    text = file.read()
 
-#for i in range(100):
-#    set_value('user' + str(i),'pass' + str(i))
-print(get_value("user1"))
+with open("database.txt", "w") as file:
+    file.write(text.replace("user1:", "row:"))
